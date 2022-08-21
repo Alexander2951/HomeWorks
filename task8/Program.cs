73c68b3e8937430a8edcb213252c18b6345b8465
Alexander <alex-lama@yandex.ru>
2022-08-21 22:25:15 +0300

    resolved

---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input number");
+double x = Convert.ToDouble(Console.ReadLine());
+int i = 1;
+while ( i < x)
+{
+    if(i%2==0) Console.Write(i+", ");
+i++;
+}
+Console.WriteLine();
+
+
