5d1c748fb311bbfc08f944dd29519616ac9f804f
Alexander <alex-lama@yandex.ru>
2022-08-21 21:55:57 +0300

    Ok!

---
+++ task6/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input number");
+double x = Convert.ToDouble(Console.ReadLine());
+if(x%2==0) Console.WriteLine(x+" - чётное число");
+else Console.WriteLine (x+" - нечётное число");
