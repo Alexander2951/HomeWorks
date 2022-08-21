9789a8da337bd4acc92f6aa7637b3bd10db9e6c4
Alexander <alex-lama@yandex.ru>
2022-08-21 21:05:31 +0300

    cogratulations

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число!");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int c= Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.WriteLine("Самое большое число "+max);
