518b6910a9680af1f580404554a6fb6f5c385c1b
Alexander <alex-lama@yandex.ru>
2022-08-21 21:30:14 +0300

    It's decided

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int oneNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int tooNumber = Convert.ToInt32(Console.ReadLine());
+if (oneNumber < tooNumber) Console.WriteLine(oneNumber +" меньше "+ tooNumber);
+if (oneNumber > tooNumber) Console.WriteLine(oneNumber +" больше "+ tooNumber );
+if (oneNumber == tooNumber) Console.WriteLine("Опять обманули !!!" );
